3b3ed69632923d433483db30c08d2691a53d1551
Tyrell Perera <tyrell.perera@gmail.com>
2021-05-16 14:48:10 +1000

    Adding contract, deployment scripts and NFT meta-data.

---
+++ scripts/deploy.js
@@ -0,0 +1,21 @@
+/**
+ * Deploy ERC-721 contract to the Ethereum blockchain.
+ * 
+ * @author Tyrell Perera
+ * 
+ */
+
+async function main() {
+  const MyNFT = await ethers.getContractFactory("MyNFT");
+
+  // Start deployment, returning a promise that resolves to a contract object
+  const myNFT = await MyNFT.deploy();
+  console.log("Contract deployed to address:", myNFT.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
